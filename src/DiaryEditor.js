0d87fc49a98d93b905c156960ba5285f4846a3b6
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-30 18:48:33 +0900

    useRef를 활용해서 DOM요소에 접근

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const DiaryEditor = () => {
   const [state, setState] = useState({
@@ -7,9 +7,12 @@ const DiaryEditor = () => {
     emotion: 1,
   });
 
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const handleChangeState = (e) => {
-    console.log(e.target.value);
-    console.log(e);
+    // console.log(e.target.value);
+    // console.log(e);
     setState({
       ...state,
       [e.target.name]: e.target.value,
@@ -17,28 +20,38 @@ const DiaryEditor = () => {
     });
   };
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
     alert("저장 성공");
-    // 나중에 이거 state 배열에 객체 추가 붙여주고 map으로 리스트 뽑을듯?
+    console.log(state);
   };
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
         />
         <div>
           <textarea
+            ref={contentInput}
             name="content"
             value={state.content}
             onChange={handleChangeState}
           />
         </div>
-        <input />
+        {/* <input /> */}
         <div>
+          <span>오늘의 감정점수 : </span>
           <select
             name="emotion"
             value={state.emotion}
