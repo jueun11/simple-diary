dc3483ae285da8e2f799e5e3dac6b3c8c05333fd
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-31 22:13:51 +0900

    부모 컴포넌트에서 props받아서 자식으로 전달

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ diaryList, onRemove, onEdit }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       {diaryList.map((el) => (
-        <DiaryItem key={el.id} {...el} />
+        <DiaryItem key={el.id} {...el} onRemove={onRemove} onEdit={onEdit} />
       ))}
     </div>
   );
