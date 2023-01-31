0240f5008bd8057a4d08e5a4149641aad55ca2dd
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-31 22:50:42 +0900

    Lifecycle useEffect로 제어
    생성시 한번만, 업데이트 될 때마다의 경우

---
+++ src/LifeCycle.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+const LifeCycle = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  useEffect(() => {
+    console.log("업데이트");
+  });
+
+  useEffect(() => {
+    console.log(`count is update : ${count}`);
+    if (count > 5) {
+      alert("count가 5를 넘었습니다. 따라서 1로 초기화 합니다.");
+      setCount(1);
+    }
+  }, [count]);
+
+  useEffect(() => {
+    console.log(`text is update : ${text}`);
+  }, [text]);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <div>
+        {count}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <input
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LifeCycle;
