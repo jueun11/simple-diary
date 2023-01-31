2102631f1d08faa08d1d66eb06874fade58853e5
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-31 23:16:32 +0900

    mount시와 unmount시 출력되는 내용 다르게

--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -1,49 +1,28 @@
 import React, { useState, useEffect } from "react";
 
-const LifeCycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
+const UnmountTest = () => {
   useEffect(() => {
-    console.log("mount");
+    console.log("mount!");
+    return () => {
+      //unmount시점에 실행
+      console.log("unmount");
+    };
   }, []);
 
-  useEffect(() => {
-    console.log("업데이트");
-  });
+  return <div>Unmount Testing Component</div>;
+};
 
-  useEffect(() => {
-    console.log(`count is update : ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다. 따라서 1로 초기화 합니다.");
-      setCount(1);
-    }
-  }, [count]);
+const LifeCycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    console.log(`text is update : ${text}`);
-  }, [text]);
+  const toggle = () => {
+    setIsVisible(!isVisible);
+  };
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <input
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
