6f48d173f8613a20656494a93487ba45edc44c25
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-02-01 21:56:08 +0900

    React.memo test

---
+++ src/OptimizeTestReactmemo.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const TextView = React.memo(({ text }) => {
+  useEffect(() => {
+    console.log(`update :: text : ${text}`);
+  });
+  return <div>{text}</div>;
+});
+
+const CountView = React.memo(({ count }) => {
+  useEffect(() => {
+    console.log(`update :: count : ${count}`);
+  });
+  return <div>{count}</div>;
+});
+
+const OptimizeTest = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  return (
+    <div style={{ padding: 50 }}>
+      <div>
+        <h2>count</h2>
+        <CountView count={count} />
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <h2>text</h2>
+        <TextView text={text} />
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+    </div>
+  );
+};
+
+export default OptimizeTest;
